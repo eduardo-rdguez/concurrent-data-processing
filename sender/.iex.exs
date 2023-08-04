ea8da01976c8153fd2f2a058ed7815d0f987dd99
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-08-04 15:53:40 -0600

    Create an application called sender to send emails to real email addresses

---
+++ sender/.iex.exs
@@ -0,0 +1,6 @@
+emails = [
+  "user01@domain.com",
+  "user02@domain.com",
+  "user03@domain.com",
+  "user04@domain.com"
+]
---
+++ sender/lib/sender.ex
@@ -0,0 +1,5 @@
+defmodule Sender do
+  @moduledoc """
+  Documentation for `Sender`.
+  """
+end
