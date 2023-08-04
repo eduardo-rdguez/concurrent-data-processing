df807ba086f59e6c5b12fcbda67cfce785c518bb
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-08-04 16:11:32 -0600

    Use Task.async_stream to manage a series of task's

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -3,19 +3,21 @@ defmodule Sender do
   Documentation for `Sender`.
   """
 
+  @async_options [
+    max_concurrency: 2,
+    ordered: false,
+    on_timeout: :kill_task
+  ]
+
   def send_email(email) do
-    Process.sleep(3000)
+    Process.sleep(3_000)
     IO.puts("Email to #{email} sent")
     {:ok, "email_sent"}
   end
 
   def notify_all(emails) do
     emails
-    |> Enum.map(fn email ->
-      Task.async(fn ->
-        send_email(email)
-      end)
-    end)
-    |> Enum.map(&Task.await/1)
+    |> Task.async_stream(&send_email/1, @async_options)
+    |> Enum.to_list()
   end
 end
