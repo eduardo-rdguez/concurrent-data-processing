bbccf158e6f9514230356b96e52c37f792d427c5
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-08-04 17:25:13 -0600

    Use Task.Supervisor to see what happens when a error occurs

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -9,6 +9,8 @@ defmodule Sender do
     on_timeout: :kill_task
   ]
 
+  def send_email("user01@domain.com" = email), do: raise("Oops, couldn't send email to #{email}")
+
   def send_email(email) do
     Process.sleep(3_000)
     IO.puts("Email to #{email} sent")
@@ -16,8 +18,8 @@ defmodule Sender do
   end
 
   def notify_all(emails) do
-    emails
-    |> Task.async_stream(&send_email/1, @async_options)
+    Sender.EmailTaskSupervisor
+    |> Task.Supervisor.async_stream_nolink(emails, &send_email/1, @async_options)
     |> Enum.to_list()
   end
 end
