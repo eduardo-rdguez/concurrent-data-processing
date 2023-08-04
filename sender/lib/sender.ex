9ac14352b1ce3a4791e634d2d5f9627efab5a5ee
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-08-04 15:55:26 -0600

    Use Task.async to return a struct containing the executed task's result

--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -2,4 +2,20 @@ defmodule Sender do
   @moduledoc """
   Documentation for `Sender`.
   """
+
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
+
+  def notify_all(emails) do
+    emails
+    |> Enum.map(fn email ->
+      Task.async(fn ->
+        send_email(email)
+      end)
+    end)
+    |> Enum.map(&Task.await/1)
+  end
 end
